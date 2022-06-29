802c9b9c87ccb9e0c0640fb8f26dc282bf6bdb50
Leeharin115 <lovehaha11@gmail.com>
2022-06-30 06:38:58 +0900

    현재 날씨 API사용힌 Weather 구현

---
+++ src/components/Weather.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Weather = ({ className, children }) => {
+  const [weather, setWeather] = useState('');
+
+  const api = {
+    key: process.env.REACT_APP_API_KEY,
+    base: 'https://api.openweathermap.org/data/2.5/weather?',
+  };
+
+  const lat = 37.56667;
+  const lon = 126.97806;
+  const url = `${api.base}lat=${lat}&lon=${lon}&appid=${api.key}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        const data = response.data;
+        setWeather({
+          id: data.weather[0].id,
+          temperature: data.main.temp,
+          main: data.weather[0].main,
+        });
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  }, []);
+
+  return (
+    <div className={className}>
+      <span>{(weather.temperature - 273.15).toFixed(2)}℃ </span>
+      <span>{weather.main}</span>
+      {children}
+    </div>
+  );
+};
+
+export default Weather;
